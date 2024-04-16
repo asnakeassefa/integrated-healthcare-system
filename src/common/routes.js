9d247c2111c54217048d3c2291e1f29263395259
asnake <asnakeassefa28@gmail.com>
2024-04-16 16:36:49 +0300

    adding routes

--- src/common/routes.js
+++ src/common/routes.js
@@ -4,10 +4,14 @@ const router = express.Router()
 
 // import routes
 const userRouter = require('../resources/user/routes')
+const refreshTokenRouter = require('../resources/refreshToken/routes')
+const patientRouter = require('../resources/patient/routes')
 // const visitRouter = require('../resources/visit/routes')
 
 // Higher level routes definition
 router.use('/user', userRouter)
+router.use('/refresh', refreshTokenRouter)
+router.use('/patient', patientRouter)
 // router.use('/visit',visitRouter)
 
 module.exports = router
