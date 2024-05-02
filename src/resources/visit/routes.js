66a1ea76a9c1ef32c07da9cb7f85387c31dd8e8c
asnake <asnakeassefa28@gmail.com>
2024-05-02 12:13:15 +0300

    get patient visit

--- src/resources/visit/routes.js
+++ src/resources/visit/routes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 // define routes
 // router.route('/createVisitHistory').post(visitController.updateLastVisit)
 router.route('/getAllVisits').get(visitController.getVisits)
-router.route('/getPatientVisit/:id').get(visitController.getPatientVisit)
+router.route('/getPatientVisit/:PatientId').get(visitController.getPatientVisit)
 router.route('/getPassedAppointment').get(visitController.getPassedAppointment)
 // router.route('/getVisitHistoriesByUserId').get(visitController.getVisitHistoriesByUserId)
 // router.route('/getVisitHistoriesByPatientId').get(visitController.getVisitHistoriesByPatientId)
