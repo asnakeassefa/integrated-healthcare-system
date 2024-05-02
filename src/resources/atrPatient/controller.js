072372321fc61ef32270c228831291f3313fffff
asnake <asnakeassefa28@gmail.com>
2024-05-02 12:39:42 +0300

    add lattest date to get user

--- src/resources/atrPatient/controller.js
+++ src/resources/atrPatient/controller.js
@@ -1,4 +1,5 @@
 const Patient = require('./model')
+const LastVisit = require('../visit/model')
 // register patient
 const registerPatient = async (req, res) => {
   try {
@@ -51,18 +52,27 @@ const getPatient = async (req, res) => {
     const { atrId } = req.params
 
     // Find the patient by ID
-    const patient = await Patient.find({atrNumber:atrId})
+    const patient = await Patient.findOne({ atrNumber: atrId })
 
     // If patient not found
     if (!patient) {
       return res.status(404).json({ error: 'Patient not found' })
     }
 
+    const lastVisit = await LastVisit.findOne({ Patient: patient._id })
+    console.log('last visit:', lastVisit.visitDate, lastVisit.nextAppointmentDate)
+    var visitDate = null
+    var nextAppointmentDate = null
+    if (lastVisit) {
+      visitDate = lastVisit.visitDate
+      nextAppointmentDate = lastVisit.nextAppointmentDate
+    }
+
     // If patient found, return it
-    res.status(200).json({ patient })
+    res.status(200).json({ patient, visitDate, nextAppointmentDate })
   } catch (error) {
     console.error('Error fetching patient:', error)
     res.status(500).json({ error: 'Internal server error' })
   }
 }
-module.exports = { registerPatient, getAllPatients, getPatient}
+module.exports = { registerPatient, getAllPatients, getPatient }
